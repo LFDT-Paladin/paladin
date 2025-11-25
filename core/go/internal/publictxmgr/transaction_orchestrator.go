dfb88b6df949cd6dd89baf4672b1374295a2851b
Matthew Whitehead <matthew.whitehead@kaleido.io>
2025-11-25 10:09:18 +0000

    Comment not updated to reflect code change
    Signed-off-by: Matthew Whitehead <matthew.whitehead@kaleido.io>

--- core/go/internal/publictxmgr/transaction_orchestrator.go
+++ core/go/internal/publictxmgr/transaction_orchestrator.go
@@ -418,7 +418,7 @@ func (oc *orchestrator) pollAndProcess(ctx context.Context) (polled int, total i
 			// as we are the only thread that writes to the submissions table, for
 			// inflight transactions we have in memory that would not be overwritten
 			// by this query.
-			additional, err = oc.runTransactionQuery(ctx, oc.p.NOTX(), true /* just the individual transactions - no duplication for bindings */, nil, q)
+			additional, err = oc.runTransactionQuery(ctx, oc.p.NOTX(), true /* retrieve the private TX bindings as well */, nil, q)
 			return true, err
 		})
 		if err != nil {
