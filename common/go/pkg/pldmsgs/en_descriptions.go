1b47781e3519a98fb444d9a13e9390a32e88a862
Andrew Richardson <andrew.richardson@kaleido.io>
2026-01-21 12:30:18 -0500

    keymanager: add keymgr_sign RPC
    Signed-off-by: Andrew Richardson <andrew.richardson@kaleido.io>

--- common/go/pkg/pldmsgs/en_descriptions.go
+++ common/go/pkg/pldmsgs/en_descriptions.go
@@ -492,6 +492,7 @@ var (
 	KeyManagerInlineConfigWallets         = pdm("KeyManagerInlineConfig.wallets", "List of wallet configurations")
 	KeyManagerInlineConfigIdentifierCache = pdm("KeyManagerInlineConfig.identifierCache", "Identifier cache configuration")
 	KeyManagerInlineConfigVerifierCache   = pdm("KeyManagerInlineConfig.verifierCache", "Verifier cache configuration")
+	KeyManagerInlineConfigDisableSignRPC  = pdm("KeyManagerInlineConfig.disableSignRPC", "True to disable the signing RPC")
 
 	// KeyManagerConfig field descriptions
 	KeyManagerConfigIdentifierCache = pdm("KeyManagerConfig.identifierCache", "Identifier cache configuration")
