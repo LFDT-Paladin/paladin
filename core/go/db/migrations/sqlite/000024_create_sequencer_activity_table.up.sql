fa9d07e0b1b818c8eeb6a1d21d8b61c0f8c2a407
Matthew Whitehead <matthew.whitehead@kaleido.io>
2026-01-28 09:28:07 +0000

    Change activity ID for sqlite
    Signed-off-by: Matthew Whitehead <matthew.whitehead@kaleido.io>

--- core/go/db/migrations/sqlite/000024_create_sequencer_activity_table.up.sql
+++ core/go/db/migrations/sqlite/000024_create_sequencer_activity_table.up.sql
@@ -1,9 +1,8 @@
 CREATE TABLE sequencer_activities (
-  "id"                        BIGINT          NOT NULL,
+  "id"                        INTEGER         PRIMARY KEY AUTOINCREMENT,
   "subject_id"                TEXT            NOT NULL,
   "timestamp"                 BIGINT          NOT NULL,
   "transaction_id"            UUID            NOT NULL,
   "activity_type"             TEXT            NOT NULL,
-  "submitting_node"           TEXT            NOT NULL,
-  PRIMARY KEY ("id")
+  "submitting_node"           TEXT            NOT NULL
 );
