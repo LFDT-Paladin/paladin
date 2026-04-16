6c192be947ce0a0b41372cfa0984abe37c49bd8e
Gabriel Indik <gabriel.indik@kaleido.io>
2026-04-16 13:01:13 -0400

    Success field only used with receipt table join
    Signed-off-by: Gabriel Indik <gabriel.indik@kaleido.io>

--- core/go/internal/txmgr/transaction_query.go
+++ core/go/internal/txmgr/transaction_query.go
@@ -17,6 +17,7 @@ package txmgr
 
 import (
 	"context"
+	"maps"
 
 	"github.com/LFDT-Paladin/paladin/common/go/pkg/i18n"
 	"github.com/LFDT-Paladin/paladin/common/go/pkg/log"
@@ -45,8 +46,13 @@ var transactionFilters = filters.FieldMap{
 	"from":           filters.StringField(`"from"`),
 	"to":             filters.HexBytesField(`"to"`),
 	"type":           filters.StringField(`"type"`),
-	"success":        filters.BooleanField(`"TransactionReceipt"."success"`),
 }
+
+var transactionFullFilters = func() filters.FieldMap {
+	m := maps.Clone(transactionFilters)
+	m["success"] = filters.BooleanField(`"TransactionReceipt"."success"`)
+	return m
+}()
 
 var dispatchFilters = filters.FieldMap{
 	"id":                  filters.StringField("id"),
@@ -190,7 +196,7 @@ func (tm *txManager) QueryTransactionsResolved(ctx context.Context, jq *query.Qu
 		P:           tm.p,
 		Table:       "transactions",
 		DefaultSort: "-created",
-		Filters:     transactionFilters,
+		Filters:     transactionFullFilters,
 		Query:       jq,
 		Finalize: func(q *gorm.DB) *gorm.DB {
 			q = q.
@@ -220,7 +226,7 @@ func (tm *txManager) QueryTransactionsFullTx(ctx context.Context, jq *query.Quer
 		P:           tm.p,
 		Table:       "transactions",
 		DefaultSort: "-created",
-		Filters:     transactionFilters,
+		Filters:     transactionFullFilters,
 		Query:       jq,
 		Finalize: func(q *gorm.DB) *gorm.DB {
 			q = q.
