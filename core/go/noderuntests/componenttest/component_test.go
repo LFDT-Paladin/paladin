3d1c9a0f73f1fa7863c108635050c996a3dbd325
Jim Zhang <jim.zhang@kaleido.io>
2026-06-01 13:00:26 -0400

    fix component test failure
    Signed-off-by: Jim Zhang <jim.zhang@kaleido.io>

--- core/go/noderuntests/componenttest/component_test.go
+++ core/go/noderuntests/componenttest/component_test.go
@@ -393,8 +393,8 @@ func TestUpdatePublicTransaction(t *testing.T) {
 		tx, err = c.PTX().GetTransactionFull(ctx, *setRes.ID())
 		require.NoError(ct, err)
 		require.Len(ct, tx.Public, 1)
-		require.NotNil(ct, tx.Public[0].Activity[0])
-		assert.Regexp(ct, "ERROR.*Intrinsic", tx.Public[0].Activity[0])
+		require.NotEmpty(ct, tx.Public[0].Activity)
+		assert.Regexp(ct, "ERROR.*Intrinsic", tx.Public[0].Activity[0].Message)
 	}, 10*time.Second, 100*time.Millisecond, "Transaction was not processed with error in time")
 
 	_, err = c.PTX().UpdateTransaction(ctx, *setRes.ID(), &pldapi.TransactionInput{
